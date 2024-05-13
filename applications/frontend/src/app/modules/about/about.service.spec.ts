e6fc84a09ef1678f9d143cf21b06fce0629bedf7
Johannes Kromer <info@pencil-me.de>
2024-05-13 15:45:52 +0200

    Unit-Tests: - extend coverage for about.service

--- applications/frontend/src/app/modules/about/about.service.spec.ts
+++ applications/frontend/src/app/modules/about/about.service.spec.ts
@@ -2,10 +2,11 @@ import { TestBed } from '@angular/core/testing';
 
 import { AboutService } from './about.service';
 import { of } from 'rxjs';
-import { TECHSTACKDATASTATE } from '@app/state/knowledge/knowledge.models';
+import {TECHSTACK, TECHSTACKDATASTATE} from '@app/state/knowledge/knowledge.models';
 import { MockStore, provideMockStore } from '@ngrx/store/testing';
 import { AppState } from '@app/state';
 import { fromKnowledge } from '@app/state/knowledge';
+import {ITechCategory} from "@modules/about/about.models";
 
 describe('AboutService', () => {
   let service: AboutService;
@@ -57,6 +58,94 @@ describe('AboutService', () => {
 
     service.store$.subscribe((data) => {
       expect(data).toEqual(techData);
+    });
+  });
+
+  describe('convertAndSortTechStack', () => {
+    it('should convert and sort tech stack correctly', () => {
+      const simpTech: TECHSTACK[] = [
+        {
+          type: 'FEDEV',
+          name: 'Angular',
+          expertise_level: 70,
+          last_usage_date: new Date('2023-02-05'),
+          flag_important: false,
+          project_count: 5,
+        },
+        {
+          type: 'FEDEV',
+          name: 'Vue',
+          expertise_level: 90,
+          last_usage_date: new Date('2023-02-05'),
+          flag_important: false,
+          project_count: 6,
+        },
+        {
+          type: 'BEDEV',
+          name: 'PHP',
+          expertise_level: 85,
+          last_usage_date: new Date('2024-02-05'),
+          flag_important: false,
+          project_count: 2,
+        },
+        {
+          type: 'BEDEV',
+          name: 'NeedsToBeNotListed',
+          expertise_level: 85,
+          last_usage_date: new Date('2015-02-05'),
+          flag_important: false,
+          project_count: 2,
+        },
+        {
+          type: 'FEDEV',
+          name: 'Bootstrap',
+          expertise_level: 90,
+          last_usage_date: new Date('2021-02-05'),
+          flag_important: false,
+          project_count: 10,
+        },
+      ];
+
+      const expectedResult: ITechCategory[] = [
+        {
+          title: 'Frontend Development',
+          data: [
+            {
+              label: 'Angular',
+              percent: 70,
+              lastTouch: '2023',
+              project_count: 5
+            },
+            {
+              label: 'Vue',
+              percent: 90,
+              lastTouch: '2023',
+              project_count: 6
+            },
+            {
+              label: 'Bootstrap',
+              percent: 90,
+              lastTouch: '2021',
+              project_count: 10
+            }
+          ],
+        },
+        {
+          title: 'Backend Development',
+          data: [
+            {
+              label: 'PHP',
+              percent: 85,
+              lastTouch: '2024',
+              project_count: 2
+            }
+          ],
+        }
+      ];
+
+      const toTest: ITechCategory[] = service.convertAndSortTechStack(simpTech);
+
+      expect(toTest).toEqual(expectedResult); // Make sure the result matches the expected result
     });
   });
 });
