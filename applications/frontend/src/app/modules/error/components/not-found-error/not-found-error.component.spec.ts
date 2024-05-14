f87082f2bce98d333f5b8def68c10553248ca551
Johannes Kromer <info@pencil-me.de>
2024-05-14 16:12:21 +0200

    Unit Tests: - extend test coverage for not-found-error.component

--- applications/frontend/src/app/modules/error/components/not-found-error/not-found-error.component.spec.ts
+++ applications/frontend/src/app/modules/error/components/not-found-error/not-found-error.component.spec.ts
@@ -1,19 +1,38 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, ComponentFixture } from '@angular/core/testing';
 import { Router } from '@angular/router';
-
 import { NotFoundErrorComponent } from './not-found-error.component';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 
 describe('NotFoundErrorComponent', () => {
   let component: NotFoundErrorComponent;
+  let fixture: ComponentFixture<NotFoundErrorComponent>;
+  let router: Router;
 
-  let routerMock: Router;
+  beforeEach(async () => {
+    const routerSpy = jasmine.createSpyObj('Router', ['navigate']);
+
+    await TestBed.configureTestingModule({
+      declarations: [NotFoundErrorComponent],
+      providers: [
+        { provide: Router, useValue: routerSpy }
+      ],
+      schemas: [NO_ERRORS_SCHEMA]
+    }).compileComponents();
+  });
 
   beforeEach(() => {
-    routerMock = TestBed.inject(Router);
-    component = new NotFoundErrorComponent(routerMock);
+    fixture = TestBed.createComponent(NotFoundErrorComponent);
+    component = fixture.componentInstance;
+    router = TestBed.inject(Router);
+    fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should navigate to home when returnToHome is called', () => {
+    component.returnToHome();
+    expect(router.navigate).toHaveBeenCalledWith(['/']);
   });
 });
