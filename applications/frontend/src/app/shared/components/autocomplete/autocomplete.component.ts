c600c95f83f2e96db23444be16e1fd261c9241d2
Johannes Kromer <info@pencil-me.de>
2025-04-06 10:33:08 +0200

    - add autocomplete

---
+++ applications/frontend/src/app/shared/components/autocomplete/autocomplete.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-autocomplete',
+  standalone: true,
+  imports: [],
+  templateUrl: './autocomplete.component.html',
+  styleUrl: './autocomplete.component.scss',
+})
+export class AutocompleteComponent {}
